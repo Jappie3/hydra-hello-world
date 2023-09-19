bbd68e3333a15d386c268bbb440b1e53714275d6
Jappie3 <jasper22034@gmail.com>
2023-09-19 09:34:39 +0200

    fix flake.lock missing

--- flake.nix
+++ flake.nix
@@ -7,9 +7,10 @@
   }: let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
   in {
-    packages.x86_64-linux.tree = nixpkgs.legacyPackages.x86_64-linux.tree;
+    # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    # packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
 
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.tree;
+    defaultPackage.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.tree;
 
     hydraJobs."master" = self.defaultPackage;
     hydraJobs."test" = pkgs.runCommand "mycommand" {} ''
